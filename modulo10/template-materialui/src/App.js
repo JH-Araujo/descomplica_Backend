bb22a865312a8a5e6bbcc5889b2ba41a52f2c2b3
Luan Pinheiro <luanspinheiro@gmail.com>
2024-12-08 16:55:46 -0300

    Tarefa Modulo 10

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,12 +1,32 @@
+import React from "react";
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#2c2f33",
+      paper: "#23272a",
+    },
+    text: {
+      primary: "#ffffff", 
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ListarTarefa />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      {/* CssBaseline aplica o tema no fundo e texto */}
+      <CssBaseline />
+      <div className="App">
+        <Header />
+        <ListarTarefa />
+      </div>
+    </ThemeProvider>
   );
 }
 
